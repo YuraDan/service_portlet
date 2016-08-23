d48ea44f301942c4224efe1116da0b59388844e1
donchenko-y <y.donchenko@sovzond.ru>
2016-08-23 16:41:27 +0300

    Fix Service for Reports New methods in JsonBuilder

--- src/main/java/ru/gradis/sovzond/portlet/controller/ReportController.java
+++ src/main/java/ru/gradis/sovzond/portlet/controller/ReportController.java
@@ -1,10 +1,14 @@
 package ru.gradis.sovzond.portlet.controller;
 
+import com.liferay.portal.kernel.json.JSONException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import ru.gradis.sovzond.util.JsonBuilder;
+
+import java.util.Map;
 
 
 /**
@@ -18,14 +22,32 @@ public class ReportController {
 
 	@RequestMapping(value = "/Services/showReport", method = RequestMethod.POST)
 	public String showReport(@RequestParam(value = "param", required = false) String param, RedirectAttributes redirectAttributes) {
-		redirectAttributes.addAttribute("param", param);
-		return "redirect:/" + getReportUrl(true);
+		String name = "UNDEFINED";
+		Map<String, Object> map = null;
+		try {
+			name = JsonBuilder.getValueFromJsonString(param, "_config_filename");
+		} catch (JSONException e) {
+			log.error(e);
+		} finally {
+			redirectAttributes.addAttribute("name", name);
+			redirectAttributes.addAttribute("param", param);
+			return "redirect:/" + getReportUrl(true);
+		}
 	}
 
 	@RequestMapping(value = "/Services/showDesigner", method = RequestMethod.POST)
 	public String showDesigner(@RequestParam(value = "param", required = false) String param, RedirectAttributes redirectAttributes) {
-		redirectAttributes.addAttribute("param", param);
-		return "redirect:/" + getReportUrl(false);
+		String name = "UNDEFINED";
+		Map<String, Object> map = null;
+		try {
+			name = JsonBuilder.getValueFromJsonString(param, "_config_filename");
+		} catch (JSONException e) {
+			log.error(e);
+		} finally {
+			redirectAttributes.addAttribute("name", name);
+			redirectAttributes.addAttribute("param", param);
+			return "redirect:/" + getReportUrl(false);
+		}
 	}
 
 	private String getUrl(String host, String port, boolean view) {
--- src/main/java/ru/gradis/sovzond/util/JsonBuilder.java
+++ src/main/java/ru/gradis/sovzond/util/JsonBuilder.java
@@ -7,9 +7,12 @@ import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.type.TypeReference;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by donchenko-y on 6/9/16.
@@ -29,14 +32,9 @@ public class JsonBuilder {
 		return null;
 	}
 
-	public static JSONObject getJsonFromString(String json) {
+	public static JSONObject getJsonFromString(String json) throws JSONException {
 		JSONFactory factory = JSONFactoryUtil.getJSONFactory();
-		try {
-			return factory.createJSONObject(json);
-		} catch (JSONException e) {
-			e.printStackTrace();
-		}
-		return null;
+		return factory.createJSONObject(json);
 	}
 
 	public static JSONObject getJsonObj() {
@@ -47,6 +45,17 @@ public class JsonBuilder {
 
 	public static String getStringFromJson(JSONObject jsonObject) {
 		return jsonObject.toString();
+	}
+
+	public static String getValueFromJsonString(String json, String key) throws JSONException {
+		return getJsonFromString(json).getString(key);
+	}
+
+	public static Map<String, Object> getMapFromJsonString(String json) throws IOException {
+		ObjectMapper mapper = new ObjectMapper();
+		Map<String, Object> map = new HashMap<String, Object>();
+		// convert JSON string to Map
+		return mapper.readValue(json, new TypeReference<Map<String, String>>() {});
 	}
 
 
