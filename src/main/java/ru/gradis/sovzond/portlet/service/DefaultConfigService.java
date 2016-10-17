d8c7c27b8bc43db268388eadbd82cd49f1d53d81
donchenko-y <y.donchenko@sovzond.ru>
2016-10-17 12:42:03 +0300

    Add DefaultConfigServer class

---
+++ src/main/java/ru/gradis/sovzond/portlet/service/DefaultConfigService.java
@@ -0,0 +1,12 @@
+package ru.gradis.sovzond.portlet.service;
+
+/**
+ * Created by donchenko-y on 17.10.16.
+ */
+public class DefaultConfigService {
+
+	public static String getDefaultConfig() {
+		return "Hello from server!!!";
+	}
+
+}
