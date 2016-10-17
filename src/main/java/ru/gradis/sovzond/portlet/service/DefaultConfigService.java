28426886583674caafd3caead9ec661bd29bf30f
donchenko-y <y.donchenko@sovzond.ru>
2016-10-17 13:43:45 +0300

    Add @Service and change package scan url in portlet-context

--- src/main/java/ru/gradis/sovzond/portlet/service/DefaultConfigService.java
+++ src/main/java/ru/gradis/sovzond/portlet/service/DefaultConfigService.java
@@ -1,8 +1,12 @@
 package ru.gradis.sovzond.portlet.service;
 
+import org.springframework.stereotype.Service;
+
 /**
  * Created by donchenko-y on 17.10.16.
  */
+
+@Service
 public class DefaultConfigService {
 
 	public static String getDefaultConfig() {
